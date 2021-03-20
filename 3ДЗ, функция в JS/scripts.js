5f68a1819db7a0f9d38ac8b873be8280f6a7b04d
MaxKotelnikov <68281987+MaxKotelnikov@users.noreply.github.com>
2021-03-20 18:50:31 +0600

    Add files via upload

---
+++ 3ДЗ, функция в JS/scripts.js
@@ -0,0 +1,40 @@
+function sum(c,d) {
+    console.log('сумма 3 и 5 равна', c + d);
+}
+sum(3,5)
+
+
+
+function sum1(a,b) {
+    console.log('сумма -3 и -5 равна', a + b);
+}
+sum1(-3,-5)
+
+
+
+function sum2(e, f) {
+    console.log('сумма -3 и 5 равна', e + f);
+}
+sum2(-3,5)
+
+
+
+let string1 = '4'
+let string2 = '5'
+let num1 = Number(string1)
+let num2 = Number(string2)
+function sum3(g, h) {
+    console.log('сумма 4 и 5 равна', g + h);
+}
+sum3(num1,num2)
+
+
+
+let string3 = '12 бегемотов'
+let string4 = '38 попугаев'
+let num3 = Number(string3)
+let num4 = Number(string4)
+function sum4(i, j) {
+    console.log('ошибка ', i + j);
+}
+sum4(num3, num4)
